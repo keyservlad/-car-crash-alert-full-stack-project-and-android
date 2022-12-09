5e0aa24fb1269895d1e492ff92b041a409e2d84b
cedrick J Randriamilamina <cedrickrandria@gmail.com>
2022-12-09 09:44:28 -0500

    server: add index.ts for the server

---
+++ server/src/app.ts
@@ -0,0 +1,47 @@
+import express from "express"
+import cookieParser from "cookie-parser" // importing the prisma instance we created.
+import logger from "morgan" // importing the prisma instance we created.
+import cors  from 'cors';
+
+import { secrets } from './utils';
+import { errorHandler } from './utils/errors';
+import './config';
+
+const app = express()
+app.use(express.json())
+app.use(cors());
+const PORT = process.env.PORT || 3000
+
+
+// Logging
+const format =
+  secrets.ENVIRONMENT === 'development'
+    ? 'dev'
+    : '[:date[clf]] :method :url :status :res[content-length] - :response-time ms';
+app.use(logger(format));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+// Routers
+app.use(router);
+
+
+// Error handler
+app.use(errorHandler);
+
+
+
+app.use((request, response, next)=> {
+
+     return response.redirect("https://" + request.headers.host + request.url);
+
+})
+// tslint:disable-next-line:no-console
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
+export default app;
+
